96718ec141e42eb62b1021adc2ecaf7c4c9f0abd
TKtkGg <yuki.miyamoto.0729@icloud.com>
2026-02-27 19:16:08 +0900

    ログイン、コンポーネント分け

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Flex, Heading, Input, Separator, Stack } from "@chakra-ui/react";
+import { Box, Flex, Heading, Input, Separator, Stack } from "@chakra-ui/react";
 import { FC, memo } from "react"
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: FC = memo(() => {
   return (
@@ -9,7 +10,7 @@ export const Login: FC = memo(() => {
         <Separator my={4} />
         <Stack spaceY={3} py={4} px={10}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>ログイン</Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
